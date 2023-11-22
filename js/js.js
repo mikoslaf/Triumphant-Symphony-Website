2d518ae90f0d0508029a0e6bc9ce5634bf9273bc
mikoslaf <mikolaj2003wojcik@gmail.com>
2023-11-22 15:07:51 +0100

    added basic files

---
+++ js/js.js
@@ -0,0 +1,23 @@
+const lazyload = (target) => {
+  const io = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const newclass = entry.target.classList[0] + "-in";
+        $(entry.target).addClass(newclass);
+        observer.disconnect();
+      }
+    });
+  });
+  io.observe(target);
+};
+
+$(function () {
+  const targets = [
+    ...document.querySelectorAll(".fade-s"),
+    ...document.querySelectorAll(".fade-e"),
+    ...document.querySelectorAll(".fade-b"),
+    ...document.querySelectorAll(".fade"),
+  ];
+  targets.forEach(lazyload);
+
+});
